57f655c6a1f0c1a59de1bda4f5d3098a6fb5077a
Otabek Akbarov <60823128+AkbarovOtabek@users.noreply.github.com>
2023-12-20 22:17:49 +0500

    Add files via upload

---
+++ orders/views.py
@@ -0,0 +1,15 @@
+from django.views.generic.edit import CreateView
+from orders.forms import OrderForm
+from common.views import TitleMixin
+from django.urls import reverse_lazy
+
+
+class OrderCreateView( TitleMixin, CreateView):
+    template_name = 'orders/order-create.html'
+    title = 'Store - оформление заказа'
+    form_class = OrderForm
+    success_url = reverse_lazy('orders:order_create')
+
+    def form_valid(self, form):
+        form.instance.initiator = self.request.user
+        return super(OrderCreateView, self). form_valid(form)
